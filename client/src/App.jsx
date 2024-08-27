ecd87341adb3a459ed8a78a2a02abe0b51e1aaac
GomezFabricio <fabricio.gomez4371@gmail.com>
2024-08-26 22:41:59 -0300

    Implementando paginas y estilos generales

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   )
 }
 
