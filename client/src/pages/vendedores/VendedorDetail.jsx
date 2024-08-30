8313466d5f1701ed733c16726e17e4b011445877
GomezFabricio <fabricio.gomez4371@gmail.com>
2024-08-30 13:00:37 -0300

    Creando los componentes para editar y ver el detalle de un vendedor

---
+++ client/src/pages/vendedores/VendedorDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VendedorDetail = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VendedorDetail
---
+++ client/src/pages/vendedores/VendedorEdit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VendedorEdit = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VendedorEdit
