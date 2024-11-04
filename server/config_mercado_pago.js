2c09d5ceb42d8a0866123cae08007fcd506fcaab
GomezFabricio <fabricio.gomez4371@gmail.com>
2024-11-03 21:16:52 -0300

    Update Mercado Pago configuration with test access token

--- server/config_mercado_pago.js
+++ server/config_mercado_pago.js
@@ -2,7 +2,7 @@ import { MercadoPagoConfig } from 'mercadopago/dist/mercadoPagoConfig.js';
 
 // Crea una instancia de configuración de Mercado Pago con tu access_token
 const mercadopago = new MercadoPagoConfig({
-  accessToken: 'TU_ACCESS_TOKEN'
+  accessToken: 'TEST-1273741858627121-110320-a07c982b52b13f4ba280d44e47b5fbe9-250056888'
 });
 
 export default mercadopago;
