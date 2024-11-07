7fc6c3aad72cc75d0e1a126ce1dca72d4f0868fa
GomezFabricio <fabricio.gomez4371@gmail.com>
2024-11-06 22:43:26 -0300

    Refactor Mercado Pago configuration: switch to CommonJS syntax and update access token handling

--- server/config_mercado_pago.js
+++ server/config_mercado_pago.js
@@ -1,10 +1,2 @@
-import { MercadoPagoConfig } from 'mercadopago';
-
-const mercadopago = new MercadoPagoConfig({
-    accessToken: 'TEST-1273741858627121-110320-a07c982b52b13f4ba280d44e47b5fbe9-250056888',
-    options: {
-        timeout: 5000,
-    },
-});
-
-export default mercadopago;
+const mercadopago = require('mercadopago');
+mercadopago.configurations.setAccessToken('TU_ACCESS_TOKEN');
