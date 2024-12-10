4201123198253b981c3af00bb88f076daee7efaa
GomezFabricio <fabricio.gomez4371@gmail.com>
2024-12-10 14:04:57 -0300

    Refactor layout styles and remove unused CSS files: update header and container styles for better spacing, add responsive design for buttons, and delete Footer and Navbar CSS files.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="bg-gray-900 text-white p-10 shadow-md w-full flex justify-center">
+
         </div>
     );
 };
